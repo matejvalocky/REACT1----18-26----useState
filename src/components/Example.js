63c8d9dd8f3bb9cfa1b211f0316b2c83a78fbdcc
Matej Valocký <matej.valocky@gmail.com>
2024-01-16 23:27:16 +0100

    react1 021

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,10 +2,37 @@ import React from 'react'
 import "./Example.css"
 
 const Example = () => {
+    // čisty javascript začiatok
+
+
+
+    const vychodziePolie = ["David", "Hermiona", "Harry"]
+
+    // const student1 = vychodziePolie[0]
+    // const student2 = vychodziePolie[1]
+    // const student3 = vychodziePolie[2]
+
+        // DESTRUCTURING
+
+        const [student1, student2, student3] = vychodziePolie
+
+        console.log(student1)
+        console.log(student2)
+        console.log(student3)
+
+
+
+
+
+
+    // čisty javascript koniec
+
+
     let title = "Môj prvý title"
 
     const buttonHandler = () => {
-        console.log("kliknuté")
+        title = "novy text v titlu"
+        console.log(title)
     }
 
     return (
